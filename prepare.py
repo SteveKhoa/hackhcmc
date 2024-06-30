8ce9f7bd98d31f62e853964bf29c358d195e2d88
Khoa LTN <vietnamkhoale@gmail.com>
2024-06-30 08:14:49 +0700

    :)

--- prepare.py
+++ prepare.py
@@ -2,7 +2,13 @@ import requests
 import os
 import re
 
-DRIVE_LINK = "https://drive.google.com/file/d/13S-KMWrQAHwoaeeMaqxFhht-PwoGUW1T/view?usp=drive_link"
+DOWNLOADS_NEEDED = {
+    "scene_augmented": "https://drive.google.com/file/d/14SkBeCkKfDOxRPWbMNZ3NUFSPWZ3beou/view?usp=sharing",
+    "scene_default": "https://drive.google.com/file/d/10UPSmVj5mm9T5P3emPf3Qcb5PDV79Ejq/view?usp=sharing",
+}
+WEIGTHT_FOLDER = "weights/"
+os.makedirs(WEIGTHT_FOLDER, exist_ok=True)
+
 
 def extract_file_id(drive_link):
     # Example drive link: https://drive.google.com/file/d/13S-KMWrQAHwoaeeMaqxFhht-PwoGUW1T/view?usp=drive_link
@@ -13,32 +19,35 @@ def extract_file_id(drive_link):
     else:
         return None
 
+
 def download_file_from_google_drive(file_id, destination):
     URL = "https://drive.google.com/uc?export=download"
 
     session = requests.Session()
 
-    response = session.get(URL, params={'id': file_id}, stream=True)
+    response = session.get(URL, params={"id": file_id}, stream=True)
     token = get_confirm_token(response)
 
     if token:
-        params = {'id': file_id, 'confirm': token}
+        params = {"id": file_id, "confirm": token}
         response = session.get(URL, params=params, stream=True)
 
     save_response_content(response, destination)
 
+
 def get_confirm_token(response):
     for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
+        if key.startswith("download_warning"):
             return value
     return None
 
+
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
-    total_size = int(response.headers.get('content-length', 0))
+    total_size = int(response.headers.get("content-length", 0))
 
     # Extract original filename from content disposition header if available
-    content_disposition = response.headers.get('content-disposition')
+    content_disposition = response.headers.get("content-disposition")
     if content_disposition:
         # Extract filename using regex
         filename = re.findall('filename="(.+)"', content_disposition)
@@ -51,13 +60,11 @@ def save_response_content(response, destination):
             if chunk:
                 f.write(chunk)
 
-# Extract the file ID
-file_id = extract_file_id(DRIVE_LINK)
 
 # Function to download the file with its original name
 def download_file_with_original_name(file_id, output_folder):
     # Download the file from Google Drive
-    output_path = os.path.join(output_folder, '')  
+    output_path = os.path.join(output_folder, "")
     download_file_from_google_drive(file_id, output_path)
 
     # Get the filename from the output path
@@ -65,13 +72,17 @@ def download_file_with_original_name(file_id, output_folder):
 
     return os.path.join(output_folder, filename)
 
-# Folder where the file will be saved
-output_folder = "weights/"
 
-# Create the folder if it doesn't exist
-os.makedirs(output_folder, exist_ok=True)
+if __name__ == "__main__":
+    # Download the file with its original name
+    # downloaded_file_path = download_file_with_original_name(
+    #     extract_file_id(DRIVE_LINK),
+    #     WEIGTHT_FOLDER,
+    # )
 
-# Download the file with its original name
-downloaded_file_path = download_file_with_original_name(file_id, output_folder)
-
-print(f"Download completed. File saved to: {downloaded_file_path}")
+    for key, url in DOWNLOADS_NEEDED.items():
+        downloaded_path = download_file_with_original_name(
+            extract_file_id(url),
+            WEIGTHT_FOLDER,
+        )
+        print(f"Download completed. File saved to: {downloaded_path}")
