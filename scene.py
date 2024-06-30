b023356dcae04fe671e37756f35a29b5a8fbc35f
Nguyen Pham Thanh Phong <thanhphong17160@gmail.com>
2024-06-30 09:21:22 +0700

    done indoor-outdoor

--- scene.py
+++ scene.py
@@ -5,6 +5,8 @@ import numpy as np
 from torchvision.transforms import v2
 from PIL import Image
 from os import sys
+from torchvision import transforms,models
+from torch import cuda, nn
 
 class SceneModel(torch.nn.Module):
     def __init__(
@@ -51,7 +53,7 @@ class MobileSceneModel(torch.nn.Module):
         x = self.transforms(x)
         logits = self.model.forward(x)
         return logits
-    
+
 
 # ASSUMPTION
 CLASSES = ['bar-pub', 'grocery', 'restaurant', 'supermarket']
@@ -74,7 +76,54 @@ def infer(model, path, classes):
 
         argmax = torch.argmax(logits, dim=1)
         pred = classes[argmax]
-    
+
+    return pred
+
+
+def predict_image(
+    model,
+    image_path,
+    class_names=["indoor", "outdoor"],
+):
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize(560),
+            transforms.CenterCrop(560 - 24),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    image = Image.open(image_path)
+    image_tensor = preprocess(image).unsqueeze(0)
+    device = cuda.device("cuda") if cuda.is_available() else "cpu"
+
+    with torch.no_grad():
+        model = model.to(device)
+        output = model(image_tensor)
+
+    _, predicted = torch.max(output, 1)
+
+    # print(f'Predicted class: {class_names[predicted.item()]}')
+    # print(predicted.item())
+    return class_names[predicted.item()]
+
+
+def predict_environment(imagepath):
+    model = models.mobilenet_v3_large(weights="DEFAULT")
+    num_classes = 2
+    model.classifier = nn.Sequential(
+        nn.Linear(in_features=960, out_features=1280, bias=True),
+        nn.Hardswish(),
+        nn.Dropout(p=0.2),
+        nn.Linear(in_features=1280, out_features=num_classes, bias=True),
+    )
+    map_loc = cuda.device("cuda") if cuda.is_available() else "cpu"
+    model.load_state_dict(
+        torch.load("models-indoor-outdoor/best_model.pth", map_location=map_loc)
+    )
+    pred = predict_image(model, imagepath)
+    # print(f"Predicted class: {pred}")
     return pred
 
 
@@ -86,4 +135,5 @@ if __name__ == "__main__":
     imagepath = sys.argv[1]
 
     pred = infer(model, imagepath, CLASSES)
-    print("Prediction: {}".format(pred))
+    pred_env = predict_environment(imagepath)
+    print(f"Prediction: {pred} - {pred_env}")
